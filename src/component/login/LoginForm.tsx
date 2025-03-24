d7fedf5fc309a178082a9091357aead14e8d69ba
JoaoMatheus <jmdelins1@outlook.com>
2025-03-24 16:07:44 -0300

    Adicionado css e função login ao login

--- src/component/login/LoginForm.tsx
+++ src/component/login/LoginForm.tsx
@@ -1,31 +1,80 @@
 import React from 'react'
-import { Link } from 'react-router'
+ import { useNavigate } from 'react-router'
+import Swal from 'sweetalert2';
+import './Login.css'
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState<string>('');
+  const navigate = useNavigate();
+
+  const [email, setEmail] = React.useState<string>('');
   const [password, setPassword] = React.useState<string>('');
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>){
-    e.preventDefault();
+  
+
+  const handleLogin = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
+    event.preventDefault();
+    try{
+      Swal.fire({
+        title: 'Cadastrando usuário...',
+        html: `
+         <div style="display: flex; flex-direction: column; align-items: center;">
+         <BarLoader color="#0000ff" width={150} />
+         <p style="margin-top: 10px;">Por favor, aguarde...</p>
+         </div>
+              `,
+         allowOutsideClick: false,
+         showConfirmButton: false,
+         willOpen: () => {
+         Swal.showLoading();
+         },
+        });
+
+    const auth = getAuth();
+    await  signInWithEmailAndPassword(auth, email, password)
+          Swal.fire({
+            icon: 'success',
+            title: 'Sucesso!',
+            text: 'Usuário cadastrado com sucesso.',
+            showConfirmButton: true,
+          }).then(() => {
+            navigate('/');
+          });
+
+    }catch(erro){
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro!',
+        text: erro as string,
+        showConfirmButton: true
+      });
+    }
   }
 
   return (
-    <section>
+  <div className='background-login-register'>
+    <div className="login">
       <h1>Login</h1>
-      <form action='' onSubmit={handleSubmit}>
+      <form>
         <input type='text'
-        value={username}
-         onChange={({target}) => setUsername(target.value)}
+        value={email}
+         onChange={({target}) => setEmail(target.value)}
+         placeholder='Insira seu e-mail'
          />
-        <input type='text'
+        <input type='password'
         value={password}
          onChange={({target}) => setPassword(target.value)}
+         placeholder='Insira sua senha'
         />
-      </form>
-        <Link to="/login/perdeu">
+        <button className='button-login' onClick={handleLogin}>Login</button>
+{/*         <Link to="/login/perdeu">
           <button>Perdeu a senha?</button>
-        </Link>
-    </section>
+        </Link> */}
+      </form>
+    </div>
+  </div>
   )
 }
 
