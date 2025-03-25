5c6af55466cbb7e779455f7ecd65059a979ae82b
JoaoMatheus <jmdelins1@outlook.com>
2025-03-25 16:59:35 -0300

    adicionado rotas privadas

--- src/Rotas.tsx
+++ src/Rotas.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
-import { Route, Routes } from 'react-router';
+import React, { useEffect, useState } from 'react'
+import { Navigate, Route, RouteProps, Routes } from 'react-router';
 import Home from './component/home/Home';
 import Login from './component/login/Login';
 import Register from './component/register/Register';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+function PrivatesRoutes(props: RouteProps){
+
+  const [loadingAuthentication, setLoadingAuthentication] = useState<boolean>(true);
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
+
+  useEffect(()=>{
+  const unsign = onAuthStateChanged(getAuth(), (user)=>{
+    console.log(user)
+    if(user === null){
+      setAuthenticated(false);
+    }else{
+      setAuthenticated(true);
+    }
+    setLoadingAuthentication(false);
+  });
+    return () => {
+      unsign();
+    };
+  }, []);
+
+  if(loadingAuthentication === true){
+    return null;
+  }else if(authenticated === false){
+    getAuth().signOut();
+    return <Navigate to= '/'/>;
+  }else{
+    return<>{props.children}</>
+  }
+}
 
 const Rotas = () => {
   return (
@@ -10,7 +41,7 @@ const Rotas = () => {
       <Routes>
         <Route path= "/" element={<Home />} />
         <Route path= "/login/*" element ={<Login/>} />
-        <Route path= "/register" element ={<Register/>}/>
+        <Route path= "/register" element ={<PrivatesRoutes><Register/></PrivatesRoutes>}/>
       </Routes> 
     </div>
   )
