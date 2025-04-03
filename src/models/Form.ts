2df261dbaac2e70c1e47a99be72a93f346744b39
JoaoMatheus <jmdelins1@outlook.com>
2025-04-03 16:11:03 -0300

    model form adicionado, não terminado

---
+++ src/models/Form.ts
@@ -0,0 +1,13 @@
+export interface IForm{
+  idForm: string,
+  titulo: string,
+  etapa: string,
+  categoriaProjeto: string,
+  dataInicio: string,
+  dataFim: string,
+  tipoPesquisa: string,
+  propriedadeIntelectual: string,
+  linkProjeto: string,
+  tipoLink: string,
+  descricaoProjeto: string,
+}
