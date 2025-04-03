51c133798663b466b7273409980bc8cb4439032b
JoaoMatheus <jmdelins1@outlook.com>
2025-04-03 16:10:30 -0300

    modificado registro de projeto para se encaixar no modelo do sistema

--- src/component/registroProjeto/RegisterProject.tsx
+++ src/component/registroProjeto/RegisterProject.tsx
@@ -55,181 +55,200 @@ function RegisterProject() {
   };
 
   return (
-    <div className="App">
-      <form className="Formulario" onSubmit={handleSubmit}>
-        <h1>Cadastro de Projeto</h1>
-        <div className="form-row">
-          <div className="form-group left-group">
-            <label>
-              Título do Projeto:
-              <input
-                type="text"
-                name="titulo"
-                value={formData.titulo}
-                onChange={handleChange}
-                required
-              />
-            </label>
+    <div className='background-login-register'>
+      <div className="App-form">
+        <form className="Formulario" onSubmit={handleSubmit}>
+          <h1>Cadastro de Projeto</h1>
+          <div className="form-row">
+            <div className="form-group left-group">
+              <label>
+                Título do Projeto:
+                <input
+                  className='input-form'
+                  type="text"
+                  name="titulo"
+                  value={formData.titulo}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div className="form-group right-group">
+              <label>
+                Etapa:
+                <select
+                  name="etapa"
+                  value={formData.etapa}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="" disabled hidden>
+                    Selecione...
+                  </option>
+                  <option value="emPlanejamento">Projeto em Planejamento</option>
+                  <option value="emDesenvolvimento">Em Desenvolvimento</option>
+                  <option value="emPublicacao">Em Publicação</option>
+                  <option value="esperandoRegistro">
+                    Aguardando Registro de Software
+                  </option>
+                  <option value="concluido">Concluído</option>
+                  <option value="cancelado">Cancelado</option>
+                </select>
+              </label>
+            </div>
           </div>
-          <div className="form-group right-group">
-            <label>
-              Etapa:
-              <select
-                name="etapa"
-                value={formData.etapa}
-                onChange={handleChange}
-                required
-              >
-                <option value="" disabled hidden>
-                  Selecione...
-                </option>
-                <option value="emPlanejamento">Projeto em Planejamento</option>
-                <option value="emDesenvolvimento">Em Desenvolvimento</option>
-                <option value="emPublicacao">Em Publicação</option>
-                <option value="esperandoRegistro">
-                  Aguardando Registro de Software
-                </option>
-                <option value="concluido">Concluído</option>
-                <option value="cancelado">Cancelado</option>
-              </select>
-            </label>
+          <div className="form-row">
+            <div className="form-group left-group">
+              <label>
+                Categoria do projeto:
+                <select
+                  name="categoria"
+                  value={formData.categoria}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="" disabled hidden>
+                    Selecione...
+                  </option>
+                  <option value="Aplicativo">Aplicativo</option>
+                  <option value="Sistema Web">Sistema Web</option>
+                  <option value="Equipamento">Equipamento</option>
+                </select>
+              </label>
+            </div>
+            <div className="form-group data-group">
+              <label>
+                Data de Início:
+                <input
+                  className='input-form'
+                  type="date"
+                  name="dataInicio"
+                  value={formData.dataInicio}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div className="form-group data-group">
+              <label>
+                Data de Fim:
+                <input
+                  className='input-form'
+                  type="date"
+                  name="dataFim"
+                  value={formData.dataFim}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="form-row">
-          <div className="form-group left-group">
-            <label>
-              Categoria do projeto:
-              <select
-                name="categoria"
-                value={formData.categoria}
-                onChange={handleChange}
-                required
-              >
-                <option value="" disabled hidden>
-                  Selecione...
-                </option>
-                <option value="Aplicativo">Aplicativo</option>
-                <option value="Sistema Web">Sistema Web</option>
-                <option value="Equipamento">Equipamento</option>
-              </select>
-            </label>
+          <div className="form-row">
+            <div className="form-group left-group">
+              <label>
+                Tipo de Pesquisa:
+                <select
+                  name="tipoPesquisa"
+                  value={formData.tipoPesquisa}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="" disabled hidden>
+                    Selecione...
+                  </option>
+                  <option value="TCC">TCC</option>
+                  <option value="PSIC">PSIC</option>
+                  <option value="PBIC">PBIC</option>
+                  <option value="Mestrado">Mestrado</option>
+                  <option value="Outro">Outro</option>
+                </select>
+              </label>
+            </div>
+            <div className="form-group right-group">
+              <label>
+                Propriedade Intelectual:
+                <select
+                  name="propriedade"
+                  value={formData.propriedade}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="" disabled hidden>
+                    Selecione...
+                  </option>
+                  <option value="desenhoIndustrial">Desenho Industrial</option>
+                  <option value="registroSoftware">Registro de Software</option>
+                  <option value="patente">Patente</option>
+                </select>
+              </label>
+            </div>
           </div>
-          <div className="form-group data-group">
-            <label>
-              Data de Início:
-              <input
-                type="date"
-                name="dataInicio"
-                value={formData.dataInicio}
-                onChange={handleChange}
-                required
-              />
-            </label>
+          <div className="form-row">
+            <div className="form-group left-group">
+              <label>
+                Link do Projeto:
+                <input
+                  className='input-form'
+                  type="text"
+                  name="link"
+                  value={formData.link}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="form-group right-group">
+              <label>
+                Tipo de Link:
+                <select
+                  name="tipoLink"
+                  value={formData.tipoLink}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="" disabled hidden>
+                    Selecione...
+                  </option>
+                  <option value="paginaWeb">Página Web</option>
+                  <option value="appStore">Aplicativo</option>
+                  <option value="outro">Outro</option>
+                </select>
+              </label>
+            </div>
           </div>
-          <div className="form-group data-group">
-            <label>
-              Data de Fim:
-              <input
-                type="date"
-                name="dataFim"
-                value={formData.dataFim}
-                onChange={handleChange}
-                required
-              />
-            </label>
+          <div className="form-row">
+            <div className="form-group">
+              <label>
+                Descrição Breve do Projeto:
+                <textarea
+                  className='textArea-form'
+                  name="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                  maxLength={550}
+                  rows={4}
+                  required
+                />
+                <div id="warning" style={{ color: warning.color }}>
+                  {warning.message}
+                </div>
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="form-row">
-          <div className="form-group left-group">
-            <label>
-              Tipo de Pesquisa:
-              <select
-                name="tipoPesquisa"
-                value={formData.tipoPesquisa}
-                onChange={handleChange}
-                required
-              >
-                <option value="" disabled hidden>
-                  Selecione...
-                </option>
-                <option value="TCC">TCC</option>
-                <option value="PSIC">PSIC</option>
-                <option value="PBIC">PBIC</option>
-                <option value="Mestrado">Mestrado</option>
-                <option value="Outro">Outro</option>
-              </select>
-            </label>
-          </div>
-          <div className="form-group right-group">
-            <label>
-              Propriedade Intelectual:
-              <select
-                name="propriedade"
-                value={formData.propriedade}
-                onChange={handleChange}
-                required
-              >
-                <option value="" disabled hidden>
-                  Selecione...
-                </option>
-                <option value="desenhoIndustrial">Desenho Industrial</option>
-                <option value="registroSoftware">Registro de Software</option>
-                <option value="patente">Patente</option>
-              </select>
-            </label>
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="form-group left-group">
-            <label>
-              Link do Projeto:
-              <input
-                type="text"
-                name="link"
-                value={formData.link}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div className="form-group right-group">
-            <label>
-              Tipo de Link:
-              <select
-                name="tipoLink"
-                value={formData.tipoLink}
-                onChange={handleChange}
-                required
-              >
-                <option value="" disabled hidden>
-                  Selecione...
-                </option>
-                <option value="paginaWeb">Página Web</option>
-                <option value="appStore">Aplicativo</option>
-                <option value="outro">Outro</option>
-              </select>
-            </label>
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="form-group">
-            <label>
-              Descrição Breve do Projeto:
-              <textarea
-                name="description"
-                value={formData.description}
-                onChange={handleChange}
-                maxLength={550}
-                rows={4}
-                required
-              />
-              <div id="warning" style={{ color: warning.color }}>
-                {warning.message}
-              </div>
-            </label>
-          </div>
-        </div>
-        <button type="submit">Enviar</button>
-      </form>
+        <label htmlFor="file-upload" className="file-input-label">
+          <span>
+            {'Selecione um arquivo PDF ou DOC'}
+          </span>
+          <div/>
+          <input
+            id="file-upload"
+            type="file"
+            accept=".pdf,.doc,.docx"
+            className="file-input"
+          />
+        </label>
+          <button className='Button-project' type="submit">Enviar</button>
+        </form>
+      </div>
     </div>
   );
 }
