ba64bf7bfaa2d51ea7b6fad2a618b881e7df37ac
JoaoMatheus <jmdelins1@outlook.com>
2025-05-09 15:14:39 -0300

    alterado para ficar em conforme com convenção

--- src/component/agendamento/agendamento.tsx
+++ src/component/agendamento/agendamento.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const agendamento = () => {
+const Agendamento = () => {
   return (
     <div>
       <h1>Teste</h1>
@@ -8,4 +8,4 @@ const agendamento = () => {
   )
 }
 
-export default agendamento
+export default Agendamento
